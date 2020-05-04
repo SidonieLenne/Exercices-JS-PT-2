ec0feddb1aebbbac8673b0973ec7bd2b0cae0bda
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-04 16:50:10 +0200

    exercice 4 fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
 //Déclare une variable qui englobe les div que je veux faire apparaître/disparaître
 var blockquotes = document.getElementById('blockquoteDiv');
+
+//Déclare une fonction qui mettra les blocs en display:none.
+function disappear(){
+    blockquotes.style.display='none';
+}
 /*window est un élément réservé à JS. 
 La fonction addEventListener lui demande littéralement d'écouter tel ou tel évènement qui seront mis en argument
-Les arguments sont donc l'évènement "scroll" et une fonction anonyme.
+Les arguments sont donc l'évènement "scroll" et une fonction anonyme 
+(on est obligé d'en mettre une mais on en a aucune à mettre alors on en met une anonyme).
 Donc SI la fenêtre est scrollée de +150px verticalement, les blocs apparaissent en display block,
-SINON, les blocs disparaissent en display none.
+SINON, les blocs disparaissent.
 */ 
 window.addEventListener("scroll",function(){
     if(window.scrollY >150){
         blockquotes.style.display='block';
     }
     else{
-        blockquotes.style.display='none';
+        disappear();
     }
-}, false);//ça je sais pas à quoi ça sert mais ça fonctionne pas sans 
+});
