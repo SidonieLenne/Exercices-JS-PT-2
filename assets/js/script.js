8ac4dc98faf0945019393f5daa16aeafbbc30768
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-04 16:35:34 +0200

    exercice 3 corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+//Je déclare ma fonction, qui fera que l'image fasse 600px de largeur.
+/*Alternative:
 function biggerImg(){
     document.getElementById('flocon').width=600;
+}*/
+
+function biggerImg(){
+    var element = document.getElementById('flocon');
+    element.style.width='600px';
+    element.style.height='600px';
 }
