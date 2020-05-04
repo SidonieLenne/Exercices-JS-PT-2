909128d0d9b9e9c52f870b46f02a8c2aca203615
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-04 14:28:39 +0200

    exercice 1 corrigé

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//La fonction Elsa et la fonction Flocon sont déclarées. La première est appelée quand la souris passe, l'autre quand la souris n'est plus.
+function changeImgElsa(){
+    document.getElementById('flocon').src='assets/images/elsa.jpg';
+}
+function changeImgFlocon(){
+    document.getElementById('flocon').src='assets/images/flocon.jpg';
+}
