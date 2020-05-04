4b52dd250dd087b5102187a1eccf06e7d8197a48
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-04 15:51:38 +0200

    exercice 2 corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Je déclare la fonction showMe, qui ouvrira une alerte qui indiquera ce qu'il y a d'écrit dans le champ de saisie.
 function showMe(){
     alert(document.getElementById('field').value)
 }
